00c89d9157a5ec1323eccd7bf6b75e8cccddeace
m1 <uzbzarif@gmail.com>
2024-11-07 16:02:21 +0500

    print attributes

--- book.py
+++ book.py
@@ -6,3 +6,14 @@ class Book:
         self.publication_year = publication_year
         self.price = price
         self.availability = availability
+
+# Create an instance of the Book class
+book = Book("Harry Potter and the Philosopher's Stone", "J.K. Rowling", "Fantasy", 1997, 10.99, True)
+
+# Accessing attributes
+print(book.title)  # Output: Harry Potter and the Philosopher's Stone
+print(book.author)  # Output: J.K. Rowling
+print(book.genre)   # Output: Fantasy
+print(book.publication_year)  # Output: 1997
+print(book.price)  # Output: 10.99
+print(book.availability)  # Output: True
